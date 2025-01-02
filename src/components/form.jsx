e3b744b7abd9482d9a2d286bf5d55e09a8eedbb5
José A. Muñoz Jiménez <jamj2000@gmail.com>
2025-01-02 22:00:29 +0100

    default state to {}

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -9,28 +9,28 @@ import { toast } from "sonner";
 
 function Formulario() {
 
-    const [state1, action1, pending1] = useActionState(realAction1, null)
-    const [state2, action2, pending2] = useActionState(realAction2, null)
-    const [state3, action3, pending3] = useActionState(realAction3, null)
+    const [state1, action1, pending1] = useActionState(realAction1, {})
+    const [state2, action2, pending2] = useActionState(realAction2, {})
+    const [state3, action3, pending3] = useActionState(realAction3, {})
 
     useEffect(() => {
-        if (state1?.error) toast.error(state1.error, { closeButton: true })       // duration: 4000
-        if (state1?.success) toast.success(state1.success, { closeButton: true }) // duration: 4000
+        if (state1.error) toast.error(state1.error, { closeButton: true })       // duration: 4000
+        if (state1.success) toast.success(state1.success, { closeButton: true }) // duration: 4000
     }, [state1])
 
     useEffect(() => {
-        if (state2?.error) toast.error(state2.error, { duration: 2000 })
-        if (state2?.success) toast.success(state2.success, { duration: 2000 })
+        if (state2.error) toast.error(state2.error, { duration: 2000 })
+        if (state2.success) toast.success(state2.success, { duration: 2000 })
     }, [state2])
 
     useEffect(() => {
-        if (state3?.error) toast.error(state3.error, { closeButton: true, duration: 2000 })
-        if (state3?.success) toast.success(state3.success, { closeButton: true, duration: 2000 })
+        if (state3.error) toast.error(state3.error, { closeButton: true, duration: 2000 })
+        if (state3.success) toast.success(state3.success, { closeButton: true, duration: 2000 })
     }, [state3])
 
 
     return (
-        <form action={action1} className="my-20 border-2 p-4 flex flex-col gap-4">
+        <form className="my-20 border-2 p-4 flex flex-col gap-4">
             <h1 className="text-center text-xl">Formulario</h1>
             <div className="flex justify-between">
                 <label htmlFor="nombre">Nombre:</label>
@@ -44,7 +44,7 @@ function Formulario() {
             {/* Si el botón no tiene propiedad formAction, 
             entonces usa la action indicada en etiqueta form */}
             <button
-                // formAction={action1}
+                formAction={action1}
                 disabled={pending1}
                 className="disabled:bg-slate-600 bg-blue-600 text-white rounded-lg py-2" >
                 {pending1 ? <RefreshCw className="inline animate-spin size-4" /> : 'Action 1'}
